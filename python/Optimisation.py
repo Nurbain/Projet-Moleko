97a396065e9e5027797a6fc4ca87cf49098d7e49
Nurbain <nathan.urbain@etu.unistra.fr>
2016-07-06 15:19:27 +0200

    Ajout du script de remplacement de valence par celle importée

--- python/Optimisation.py
+++ python/Optimisation.py
@@ -251,6 +251,57 @@ def scaling() :
 		bpy.ops.mesh.select_all(action = 'SELECT')
 		bpy.ops.mesh.normals_make_consistent(inside=False)
 		bpy.ops.object.mode_set(mode='OBJECT')		
+
+
+def changementvalence():
+#sélectionne tout
+	bpy.ops.object.select_grouped(type='TYPE')
+#liste modele
+	listev = []
+	listem = []
+#ajoute dans la liste les objets modele
+	for objet in bpy.context.selected_objects :
+		bpy.context.scene.objects.active = objet
+#on vérifie si le nom de l'objet contient Hydro , pour vérifier que c'est un hydrogène 
+		if 'Valence' in objet.name:
+			bpy.context.scene.objects.active = objet
+			listev.append(bpy.context.active_object)
+		if 'Model' in objet.name:
+			bpy.context.scene.objects.active = objet
+			model = bpy.context.active_object
+#création des nbr objets
+	bpy.ops.object.select_all(action='DESELECT')
+	bpy.context.scene.objects.active = model
+	model.select=True
+	for k in range(0,len(listev)):
+		bpy.ops.object.duplicate_move()
+		obj=bpy.context.active_object
+		listem.append(obj)
+#valeur absolue du scaling
+		x=listev[k].scale.x
+		y=listev[k].scale.y
+		z=listev[k].scale.z
+#modification scaling
+		obj.scale = [x,z,y]
+		print(obj.name)
+#modification location
+		obj.location = [listev[k].location.x,listev[k].location.y,listev[k].location.z]
+#modification rotation, le 1.5708 rad correspond a 90° 
+		obj.rotation_euler = [listev[k].rotation_euler.x+1.5708,listev[k].rotation_euler.y,listev[k].rotation_euler.z]
+#smooth de l'objet
+		bpy.ops.object.shade_smooth()
+#couleur de l'objet
+#supression des modèles 
+	for l in range(0,len(listem)) :
+		bpy.ops.object.select_all(action='DESELECT')
+		listem[l].select=True
+		bpy.context.scene.objects.active = listem[l]
+		scaling()
+	bpy.ops.object.select_all(action='DESELECT')
+	for n in range(0,len(listev)):
+		listev[n].select = True
+		bpy.ops.object.delete(use_global=False)
+
 		
 #Fonction général effectuant le travaille , main 
 def optimisation():
@@ -259,4 +310,5 @@ def optimisation():
 	changementgeneral()
 	changementliaisons()
 	changementhydro()
-	
+	changementvalence()
+
