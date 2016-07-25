33dd422a7f23ae0eb55822f72d8cbdab247d5a3b
Nurbain <nathan.urbain@etu.unistra.fr>
2016-07-25 17:22:42 +0200

    Maj fichier pour application PhoneGap

---
+++ js/OngletApp.js
@@ -0,0 +1,31 @@
+var etatL = 0
+
+function OngletLeft()
+{	
+	if (etatL == 0 )
+	{
+		document.getElementById('left').style.visibility = "visible";
+		etatL = 1;
+	}
+	else 
+	{
+		document.getElementById('left').style.visibility = "hidden";
+		etatL = 0;
+	}
+}
+
+var etatR = 0
+
+function OngletRight()
+{	
+	if (etatR == 0)
+	{
+		document.getElementById('right').style.visibility = "visible";
+		etatR = 1;
+	}
+	else 
+	{
+		document.getElementById('right').style.visibility = "hidden";
+		etatR = 0;
+	}
+}
