ed18b4c6f316fb143c2340a73f146073aa2ab575
Nurbain <nathan.urbain@etu.unistra.fr>
2016-07-26 10:51:14 +0200

    1er visuel onglet application

--- js/OngletApp.js
+++ js/OngletApp.js
@@ -1,11 +1,22 @@
 var etatL = 0
+var etatR = 0
 
 function OngletLeft()
 {	
 	if (etatL == 0 )
 	{
+		if (etatR == 1)
+		{
+		document.getElementById('right').style.visibility = "hidden";
+		etatR = 0;
 		document.getElementById('left').style.visibility = "visible";
 		etatL = 1;
+		}
+		else 
+		{
+			document.getElementById('left').style.visibility = "visible";
+			etatL = 1;
+		}
 	}
 	else 
 	{
@@ -14,14 +25,22 @@ function OngletLeft()
 	}
 }
 
-var etatR = 0
-
 function OngletRight()
 {	
 	if (etatR == 0)
 	{
-		document.getElementById('right').style.visibility = "visible";
-		etatR = 1;
+		if (etatL == 1)
+		{
+			document.getElementById('left').style.visibility = "hidden";
+			etatL = 0;
+			document.getElementById('right').style.visibility = "visible";
+			etatR = 1;
+		}
+		else 
+		{
+			document.getElementById('right').style.visibility = "visible";
+			etatR = 1;
+		}
 	}
 	else 
 	{
@@ -29,3 +48,4 @@ function OngletRight()
 		etatR = 0;
 	}
 }
+
